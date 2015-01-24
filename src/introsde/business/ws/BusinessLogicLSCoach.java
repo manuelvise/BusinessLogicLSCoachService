6c3a03c2888ed60f06b7247a31bb40471c919c03
manuelvise <manuelvise@gmail.com>
2015-01-24 16:44:21 +0100

    init

---
+++ src/introsde/business/ws/BusinessLogicLSCoach.java
@@ -0,0 +1,23 @@
+package introsde.business.ws;
+
+import introsde.business.model.Activities;
+import introsde.business.model.Profile;
+import introsde.business.model.Weights;
+
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.WebResult;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
+
+@WebService
+@SOAPBinding
+public interface BusinessLogicLSCoach {
+	
+
+
+   }
---
+++ src/introsde/business/ws/BusinessLogicLSCoachImpl.java
@@ -0,0 +1,23 @@
+package introsde.business.ws;
+
+import introsde.business.model.Activities;
+import introsde.business.model.Profile;
+import introsde.business.model.Weights;
+import introsde.business.util.HttpGetRequestRK;
+import introsde.business.util.RunKConstants;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+
+//Service Implementation
+
+@WebService(endpointInterface = "introsde.business.ws.BusinessLogicLSCoach", serviceName = "BusinessLogicLSCoachService")
+public class BusinessLogicLSCoachImpl implements BusinessLogicLSCoach {
+
+
+
+}
