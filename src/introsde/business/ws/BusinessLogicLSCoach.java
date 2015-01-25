797744bd5e79fa7b83d6b73b5c07b233ef42027d
manuelvise <manuelvise@gmail.com>
2015-01-25 15:32:23 +0100

    manage remote user and remote weights

--- src/introsde/business/ws/BusinessLogicLSCoach.java
+++ src/introsde/business/ws/BusinessLogicLSCoach.java
@@ -3,6 +3,8 @@ package introsde.business.ws;
 import introsde.business.model.Activities;
 import introsde.business.model.Profile;
 import introsde.business.model.Weights;
+import introsde.storage.ws.MeasureDefinition;
+import introsde.storage.ws.Person;
 
 import java.util.List;
 
@@ -18,6 +20,19 @@ import javax.jws.soap.SOAPBinding.Use;
 @SOAPBinding
 public interface BusinessLogicLSCoach {
 	
+	@WebMethod(operationName="syncWeightToDB")
+    @WebResult(name="fitnessActivities") 
+    public Boolean syncWeightsToDB(@WebParam(name="accessToken") String accessToken);
+	
+	@WebMethod(operationName="syncPersonToDB")
+    @WebResult(name="personSynchronized") 
+    public Person syncPersonToDB(@WebParam(name="accessToken") String accessToken);
+	
+	@WebMethod(operationName="setupNewDefinitioMeasure")
+    @WebResult(name="newMeasureDefinition") 
+    public MeasureDefinition setupNewDefinitioMeasure(@WebParam(name="name") String name, @WebParam(name="type") String type);
 
 
+	
    }
+
--- src/introsde/business/ws/BusinessLogicLSCoachImpl.java
+++ src/introsde/business/ws/BusinessLogicLSCoachImpl.java
@@ -1,15 +1,13 @@
 package introsde.business.ws;
-
-import introsde.business.model.Activities;
-import introsde.business.model.Profile;
-import introsde.business.model.Weights;
-import introsde.business.util.HttpGetRequestRK;
-import introsde.business.util.RunKConstants;
-
-import java.io.IOException;
 import java.util.List;
 
-import javax.jws.WebParam;
+import introsde.storage.ws.HealthMeasureHistory;
+import introsde.storage.ws.Measure;
+import introsde.storage.ws.MeasureDefinition;
+import introsde.storage.ws.People;
+import introsde.storage.ws.PeopleStorageService;
+import introsde.storage.ws.Person;
+
 import javax.jws.WebService;
 
 
@@ -17,6 +15,65 @@ import javax.jws.WebService;
 
 @WebService(endpointInterface = "introsde.business.ws.BusinessLogicLSCoach", serviceName = "BusinessLogicLSCoachService")
 public class BusinessLogicLSCoachImpl implements BusinessLogicLSCoach {
+
+	private People storageServicePeople;
+	
+	private static final String WEIGHT_TYPE = "weight";
+	private static final String STRING_TYPE = "String";
+	
+	public BusinessLogicLSCoachImpl() {		
+		PeopleStorageService serviceStorage = new PeopleStorageService();
+		storageServicePeople = serviceStorage.getPeopleImplPort();
+	}
+	
+	@Override
+	public Boolean syncWeightsToDB(String accessToken) {
+		
+		List<HealthMeasureHistory> listWeightsMeasure = storageServicePeople.readPersonRemoteWeightHistory(accessToken);
+
+		Long pId = storageServicePeople.readRemotePersonId(accessToken);
+		Person p = storageServicePeople.readPerson(pId);
+		
+		if(p==null){
+			p = syncPersonToDB(accessToken);
+		}
+			
+		MeasureDefinition weightDefinition = storageServicePeople.getCompleteMeasureTypeFromName(WEIGHT_TYPE);
+		if(weightDefinition==null){
+			weightDefinition = setupNewDefinitioMeasure(WEIGHT_TYPE, STRING_TYPE);
+		}
+		
+		for (HealthMeasureHistory healthMeasureHistory : listWeightsMeasure) {
+			
+			Measure measure = new Measure();
+			measure.setMeasureDefinition(weightDefinition);
+			measure.setTimestamp(healthMeasureHistory.getTimestamp());
+			measure.setValue(healthMeasureHistory.getValue());
+			
+			
+			storageServicePeople.savePersonMeasurement(p.getIdPerson(), measure);
+		}
+		
+		return true;
+	}
+
+	@Override
+	public Person syncPersonToDB(String accessToken) {
+		
+		Person personRemote = storageServicePeople.readRemotePerson(accessToken);
+		
+		Long id = storageServicePeople.createPerson(personRemote);
+		return storageServicePeople.readPerson(id);
+		
+	}
+
+	@Override
+	public MeasureDefinition setupNewDefinitioMeasure(String name, String type) {
+		MeasureDefinition newMeasureDef = new MeasureDefinition();
+		newMeasureDef.setMeasureName(name);
+		newMeasureDef.setMeasureType(type);
+		return storageServicePeople.saveMeasureDefinition(newMeasureDef);
+	}
 
 
 
